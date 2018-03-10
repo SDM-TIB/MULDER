ccc6c6c7ddce75b3263ab1c9bd5f3ed1ec6fe7f8
keme686 <Keme702755>
2018-03-10 14:32:40 +0100

    run script to 5 repetitions

--- runQueries.sh
+++ runQueries.sh
@@ -6,8 +6,12 @@ if [ "$#" -lt 5 ]; then
 fi
 
 echo -e  "qname,decompositionTime,planningTime,firstResult,overallExecTime,status,cardinality" >> $3
-
-for query in `ls -v $1/*`; do
-    (timeout -s 12 300 start_experiment.py -c $2 -q $query -t $5 -s True ) 2>> $4 >> $3;
-
+for n in {1..5}; do
+    for query in `ls -v $1/*`; do
+        (timeout -s 12 300 start_experiment.py -c $2 -q $query -t $5 -s True ) 2>> $4 >> $3;
+        # kill any remaining processes
+        # pkill -9 start_experiment.py
+        # kill -9 $(pidof start_experiment.py)
+        killall -9 --quiet start_experiment.py
+    done;
 done;
